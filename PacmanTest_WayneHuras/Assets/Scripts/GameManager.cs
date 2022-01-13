cc0360cb6a4fb2ff96877f6a44b45d91ce97204d
Unknown <wayne.stamand@hotmail.com>
2022-01-13 14:48:03 -0500

    Create GameManager, Welcome, Level, and GameOver scenes
    Create GameManager script and various screen required for game flow.

---
+++ PacmanTest_WayneHuras/Assets/Scripts/GameManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance;
+    public static GameManager Instance() => instance;
+
+    private void Awake()
+    {
+        MaintainSingleton();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SceneManager.LoadScene("Welcome");
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            SceneManager.LoadScene("Level");
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            SceneManager.LoadScene("GameOver");
+        }
+    }
+
+    private void MaintainSingleton()
+    {
+        if(instance != null && instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
